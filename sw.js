fa2cdd6d853fd666c65f98685059d6514520fb05
mauricchio91 <maurizio.auricchio91@gmail.com>
2026-08-31 00:08:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Strategia: prima la rete con un tetto di 2,5 secondi, poi la copia locale.
    In una sala con connessione ballerina è la scelta giusta: non resti mai
    appeso ad attendere, ma quando c'è linea prendi la versione aggiornata. */
-var CACHE = "asta-live-e97c4a72";
+var CACHE = "asta-live-0affad38";
 var FILES = ["./", "./index.html", "./manifest.webmanifest"];
 
 /* caches.addAll è atomica: se anche un solo file risponde 404 l'intera Promise
